66ee52870ef1650c433bbffaf534f8aa930c4cba
yarden <yarden@omicron.co.il>
2021-07-29 10:39:15 +0300

    completed test

--- story-app/src/app/app-routing.module.ts
+++ story-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WhiteboardPageComponent } from './whiteboard-page/whiteboard-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'whiteboard', component: WhiteboardPageComponent },
+  { path: '**', component: WhiteboardPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- story-app/src/app/app.module.ts
+++ story-app/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WhiteboardPageComponent } from './whiteboard-page/whiteboard-page.component';
+import { ShapeService } from 'src/services/shape.service';
+import { TextNodeService } from 'src/services/text-node.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WhiteboardPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    ShapeService,
+    TextNodeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
